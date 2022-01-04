a78abbbc0a83efcb4c62e21a62bfd31bbc62583b
toffee2000 <70193215+toffee2000@users.noreply.github.com>
2022-01-04 21:08:37 +0800

    added PFD flaps, gear ind.

--- HUD.gd
+++ HUD.gd
@@ -5,6 +5,9 @@ var display_roll = 0
 var display_spd = 0
 var display_alt = 0
 var display_hdg = 0
+var display_flaps = 0
+var display_trim = 0
+var display_gear = 0
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -27,12 +30,16 @@ func _process(delta):
 	display_spd = $'../../'.pfd_spd
 	display_hdg = $'../../'.pfd_hdg
 	display_alt = $'../../'.pfd_alt
+	display_flaps = $'../../'.flaps_input
+	display_trim = $'../../'.trim_pitch_input
+	display_gear = $'../../'.gear_pos
 	
 	$Text_Line_1/Speed_Data/Variable.text = "%03d" % stepify($'../../'.pfd_spd, 1)
 	$Text_Line_1/Alt_Data/Variable.text = "%05d" % stepify($'../../'.pfd_alt, 1)
 	$Text_Line_1/Heading_Data/Variable.text = "%03d" % stepify($'../../'.pfd_hdg, 1)
-	$Text_Line_2/Flaps_Data/Variable.text = "%03d" % stepify($'../../'.flaps_input, 0.01)
-	$Text_Line_2/Gear_Data/Variable.text = "%03d" % stepify($'../../'.gear_pos, 0.01)
+	$Text_Line_2/Flaps_Data/Variable.text = "%.2f" % stepify($'../../'.flaps_input, 0.01)
+	$Text_Line_2/Gear_Data/Variable.text = "%.2f" % stepify($'../../'.gear_pos, 0.01)
+	
 	
 	get_node("EADI_Mask/EADI_Image").rotation_degrees = -display_roll
 	get_node("EADI_Mask/EADI_Image").position.y = display_pitch / 90 * 260
@@ -41,3 +48,11 @@ func _process(delta):
 	get_node("EADI_Mask/Box_SPD").text = "%03d" % stepify($'../../'.pfd_spd, 1)
 	get_node("EADI_Mask/Box_ALT").text = "%05d" % stepify($'../../'.pfd_alt, 1)
 	get_node("EADI_Mask/Box_HDG").text = "%03d" % stepify($'../../'.pfd_hdg, 1)
+	
+	get_node("EADI_Mask/Box_TRIM").text = "%.2f" % stepify(display_trim, 0.01)
+	get_node("EADI_Mask/Box_FLAPS").text = "%.2f" % stepify(display_flaps, 0.01)
+	
+	if (display_gear == 0):
+		get_node("EADI_Mask/Gear_Indicator").default_color = Color8(22, 222, 22)
+	if (display_gear > 0):
+		get_node("EADI_Mask/Gear_Indicator").default_color = Color8(222, 222, 22)
