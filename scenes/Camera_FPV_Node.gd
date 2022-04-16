6157a930d2a8f3f0c32b21556c1369427785ab0b
toffee2000 <70193215+toffee2000@users.noreply.github.com>
2022-04-16 17:42:39 +0800

    fixed cam zoom bugs

--- scenes/Camera_FPV_Node.gd
+++ scenes/Camera_FPV_Node.gd
@@ -15,7 +15,7 @@ export (float) var max_zoom = 3.0
 export (float) var min_zoom = 0.4
 export (float, 0.05, 1.0) var zoom_speed = 0.09
 
-var zoom = 1.5
+var zoom = 1.0
 
 func _unhandled_input(event):
 	if Input.is_action_pressed("cam_slew"):
@@ -25,15 +25,11 @@ func _unhandled_input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
 		return
-	if event.is_action_pressed("cam_zoom_in"):
-		zoom -= zoom_speed
-	if event.is_action_pressed("cam_zoom_out"):
-		zoom += zoom_speed
-	zoom = clamp(zoom, min_zoom, max_zoom)
+
 	if mouse_control and event is InputEventMouseMotion:
 		if event.relative.x != 0:
 			var dir = 1 if invert_x else -1
-			rotate_object_local(Vector3.UP, dir * event.relative.x * mouse_sensitivity)
+			$Gimbal_X.rotate_object_local(Vector3.UP, dir * event.relative.x * mouse_sensitivity)
 		if event.relative.y != 0:
 			var dir = 1 if invert_y else -1
 			var y_rotation = clamp(event.relative.y, -30, 30)
@@ -41,28 +37,29 @@ func _unhandled_input(event):
 
 func get_input_keyboard(delta):
 	# Rotate outer gimbal around y axis
-	var y_rotation = 0
 	if Input.is_action_pressed("cam_right"):
-		y_rotation += 1
+		$Gimbal_X/Gimbal_Y.rotation.y += 0.01
 	if Input.is_action_pressed("cam_left"):
-		y_rotation += -1
-	rotate_object_local(Vector3.UP, y_rotation * rotation_speed * delta)
+		$Gimbal_X/Gimbal_Y.rotation.y -= 0.01
+		
 	# Rotate inner gimbal around local x axis
-	var x_rotation = 0
 	if Input.is_action_pressed("cam_up"):
-		x_rotation += -1
+		$Gimbal_X.rotation.x += 0.01
 	if Input.is_action_pressed("cam_down"):
-		x_rotation += 1
-	x_rotation = -x_rotation if invert_y else x_rotation
-	$Gimbal_X.rotate_object_local(Vector3.RIGHT, x_rotation * rotation_speed * delta)
-
+		$Gimbal_X.rotation.x -= 0.01
+	if Input.is_action_pressed("cam_zoom_in"):
+		zoom += zoom_speed
+	if Input.is_action_pressed("cam_zoom_out"):
+		zoom -= zoom_speed
+	zoom = clamp(zoom, min_zoom, max_zoom)
 func _process(delta):
 	get_input_keyboard(delta)
 	$Gimbal_X.rotation.x = clamp($Gimbal_X.rotation.x, -1.0, 1.0)
 	$Gimbal_X/Gimbal_Y.rotation.y = clamp($Gimbal_X/Gimbal_Y.rotation.y, -1.0, 1.0)
-	scale = lerp(scale, Vector3.ONE * zoom, zoom_speed)
+	$Gimbal_X/Gimbal_Y/Camera_FPV.fov = 70 / zoom
 	if target:
 		global_transform.origin = get_node(target).global_transform.origin
 	
 	$HMD.HMD_angles.x = $Gimbal_X.rotation.x
 	$HMD.HMD_angles.y = $Gimbal_X/Gimbal_Y.rotation.y
+	$HMD.cam_fov = $Gimbal_X/Gimbal_Y/Camera_FPV.fov
