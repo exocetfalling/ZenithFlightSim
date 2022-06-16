9eb1c27e401909ce4590848ef25b831737607376
toffee2000 <70193215+toffee2000@users.noreply.github.com>
2022-06-16 19:52:27 +0800

    Update AeroEngine.gd

--- scripts/AeroEngine.gd
+++ scripts/AeroEngine.gd
@@ -13,12 +13,14 @@ var thrust_output : float = 0.00
 var vel_freestream : float = 0.00
 var vel_exhaust : float = 0.00
 
-
+export var fan_radius : float = 1.00
+var fan_area : float
+var air_density : float
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Called every physics frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	
+	pass
