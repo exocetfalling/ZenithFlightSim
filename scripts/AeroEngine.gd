6617dba7b2437d42bda709aa264cbe1674076f15
toffee2000 <70193215+toffee2000@users.noreply.github.com>
2022-06-16 12:09:16 +0800

    Added AeroEngine class

---
+++ scripts/AeroEngine.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+class_name AeroEngine
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Thrust in N
+var thrust_rated : float = 1000
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
