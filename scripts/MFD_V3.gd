817de0ed631d6da44c08d4f1008c6a66f4bf1767
toffee2000 <70193215+toffee2000@users.noreply.github.com>
2022-10-25 11:18:45 +0800

    Added procedural speed tape
    Rough but working

--- scripts/MFD_V3.gd
+++ scripts/MFD_V3.gd
@@ -5,6 +5,8 @@ extends TabContainer
 # var a = 2
 # var b = "text"
 
+var tape_spd_ref = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -22,6 +24,18 @@ func _process(delta):
 	
 	if (FlightData.aircraft_ap == 0): 
 		pass
+	
+	tape_spd_ref = stepify(FlightData.aircraft_spd_indicated, 50)
+	$PFD/EADI/Tape_SPD/REF.text = ("%03d" % [tape_spd_ref])
+	$PFD/EADI/Tape_SPD/ABV.text = ("%03d" % [tape_spd_ref + 50])
+	$PFD/EADI/Tape_SPD/BLW.text = ("%03d" % [tape_spd_ref - 50])
+	
+	if(tape_spd_ref <= 0): 
+		$PFD/EADI/Tape_SPD/BLW.visible = false
+	else:
+		$PFD/EADI/Tape_SPD/BLW.visible = true
+	
+	$PFD/EADI/Tape_SPD.position.y = 500 + (FlightData.aircraft_spd_indicated - tape_spd_ref) * (300/50)
 	
 	get_node("PFD/EADI/Viewport/XForm_Roll").rotation_degrees = -FlightData.aircraft_roll
 	get_node("PFD/EADI/Viewport/XForm_Roll/XForm_Pitch").position.y = FlightData.aircraft_pitch * 20
