da776d10ef95774935be41fd51557fbfb32427b8
toffee2000 <70193215+toffee2000@users.noreply.github.com>
2023-01-02 11:12:29 +0800

    Revert "Added missile AP and better range terrain"
    This reverts commit cb07e2db3c8c9a908b95d7dde5c6451ac311012c.

--- scenes/Missile.gd
+++ scenes/Missile.gd
@@ -25,7 +25,6 @@ var pos_fin_r2 : Vector3 = Vector3.ZERO
 var pos_fin_r3 : Vector3 = Vector3.ZERO
 var pos_fin_r4 : Vector3 = Vector3.ZERO
 
-var cmd_vector : Vector3 = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -100,8 +99,6 @@ func _physics_process(delta):
 	pos_fin_r3 = $AeroSurface_Fin_R3.pos_force_rel
 	pos_fin_r4 = $AeroSurface_Fin_R4.pos_force_rel
 	
-	cmd_vector.z = -0.01 * calc_autopilot_factor(air_pressure_dynamic) * adc_roll
-	
 	$AeroSurface_Fin_F1.rotation = \
 		Vector3( \
 			(0.1 * (output_elevator - output_rudder)), \
@@ -140,7 +137,7 @@ func _physics_process(delta):
 	
 	$AeroSurface_Fin_R1.rotation = \
 		Vector3( \
-			(-0.1 * (cmd_vector.z)), \
+			(-0.1 * (output_aileron)), \
 			0, \
 			($AeroSurface_Fin_R1.rotation.z) \
 			)\
@@ -149,7 +146,7 @@ func _physics_process(delta):
 	
 	$AeroSurface_Fin_R2.rotation = \
 		Vector3( \
-			(+0.1 * (cmd_vector.z)), \
+			(+0.1 * (output_aileron)), \
 			0, \
 			($AeroSurface_Fin_R2.rotation.z) \
 			)\
@@ -158,7 +155,7 @@ func _physics_process(delta):
 	
 	$AeroSurface_Fin_R3.rotation = \
 		Vector3( \
-			(-0.1 * (cmd_vector.z)), \
+			(-0.1 * (output_aileron)), \
 			0, \
 			($AeroSurface_Fin_R3.rotation.z) \
 			)\
@@ -167,7 +164,7 @@ func _physics_process(delta):
 	
 	$AeroSurface_Fin_R4.rotation = \
 		Vector3( \
-			(+0.001 * (cmd_vector.z)), \
+			(+0.1 * (output_aileron)), \
 			0, \
 			($AeroSurface_Fin_R4.rotation.z) \
 			)\
