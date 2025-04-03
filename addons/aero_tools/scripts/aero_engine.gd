e2f214113f84657af6626a9dbe1a730fe0f0a177
toffee2000 <toffee2000@gmail.com>
2025-04-03 10:58:51 +0800

    Add WIP engine calcs

--- addons/aero_tools/scripts/aero_engine.gd
+++ addons/aero_tools/scripts/aero_engine.gd
@@ -14,17 +14,49 @@ var thrust_output_vector : Vector3 = Vector3.ZERO
 var throttle_current : float = 0.00
 
 var vel_freestream : float = 0.00
-@export var vel_exhaust : float = 0.00
+var vel_exhaust : float = 0.00
 var vel_delta : float = 0.00
+
+var press_freestream : float = 0.00
+var press_combustion : float = 0.00
+var press_delta : float = 0.00
+
+var temp_freestream : float = 0.00
+var temp_combustion : float = 0.00
+var temp_delta : float = 0.00
+
+var vol_freestream : float = 0.00
+var vol_exhaust : float = 0.00
+var vol_delta : float = 0.00
+
+var air_mass_flow_rate: float = 0
 
 @export var fan_radius : float = 1.00
 var fan_area : float = 3.14
+
+@export var inlet_radius : float = 1.00
+var inlet_area : float = 3.14
+
+@export var exhaust_radius : float = 1.00
+var exhaust_area : float = 3.14
+
 var air_density : float = 1.2 
+var air_ratio_oxygen: float = 0.21
+var air_ratio_nitrogen: float = 0.79
+
+var mol_oxygen: float = 0
+var mol_nitrogen: float = 0
+
+var MOLAR_MASS_KG_OXYGEN: float = 0.032
+var MOLAR_MASS_KG_NITROGEN: float = 0.028
+var IDEAL_GAS_CONST: float = 8.314
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	fan_area = PI * pow(fan_radius, 2)
+	inlet_area = PI * pow(inlet_radius, 2)
+	exhaust_area = PI * pow(exhaust_radius, 2)
 	pass # Replace with function body.
 
 # Func. for scaling thrust given air density
@@ -46,9 +78,14 @@ func thrust_coeff_given_airspeed_calibrated():
 		return ((p3.y - p2.y) / (p3.x - p2.x)) * (vel_freestream - p2.x) + p2.y
 
 
-
 # Called every physics frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	thrust_output = 0.5 * air_density * fan_area * vel_delta * throttle_current
+	air_mass_flow_rate = air_density * inlet_area * vel_freestream
+	mol_oxygen = air_ratio_oxygen * air_mass_flow_rate / MOLAR_MASS_KG_OXYGEN
+	mol_nitrogen = air_ratio_nitrogen * air_mass_flow_rate / MOLAR_MASS_KG_NITROGEN
+	
+	press_combustion = mol_nitrogen * IDEAL_GAS_CONST * temp_combustion / vol_freestream
+	
+	thrust_output = air_mass_flow_rate * vel_delta + press_delta * exhaust_area
 	thrust_output_vector = Vector3(0, 0, -thrust_output)
 	pass
